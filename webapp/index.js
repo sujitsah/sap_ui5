2f8c99fc6755c52d6b6ab0be7efc122af846db36
sugandhabajaj <sujitsah60@gmail.com>
2020-10-08 04:16:13 +0530

    controller

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,16 @@
 sap.ui.define([
-    "sap/m/Text"
+    // "sap/m/Text",
+    "sap/ui/core/mvc/XMLView"
 
-],function(Text){
+],function(XMLView){
     "use strict";
 
-    new Text({
-        text: "hello ui5"
-    }).placeAt("content")
-});
+    XMLView.create({
+        viewName: "sap.ui.demo.wt.ui5.view.App"
+    }).then(function(oView){
+        oView.placeAt("content");
+    });
+    // new Text({
+    //     text: "hello ui5"
+    // }).placeAt("content")
+}); 
