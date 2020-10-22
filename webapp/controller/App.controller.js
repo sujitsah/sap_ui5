fff5e69b5f1d6d518aa3009d964767a6ca62e008
sugandhabajaj <sujitsah60@gmail.com>
2020-10-23 02:08:49 +0530

    json model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],function(Controller){
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+],function(Controller,MessageToast,JSONModel){
     "use strict"
     return Controller.extend("sap.ui.demo.ui5.controller.App",{
-             onshow: function(){
-                alert("hellllllllllllll");
 
+        onInit: function(){
+            //set o data model ont he view
+            var oData = {
+                "rec":{
+                    "name":"sujit"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+        },
+             onshow: function(){
+                // alert("hellllllllllllll");
+                MessageToast.show("hello");
         }
     });
 
