51662980409f56b3d0742da3ee035f1c6da5f12a
sugandhabajaj <sujitsah60@gmail.com>
2020-10-26 03:42:47 +0530

    i18n tranlate text

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,24 +1,37 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-],function(Controller,MessageToast,JSONModel){
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+],function(Controller,MessageToast,JSONModel,ResourceModel){
     "use strict"
     return Controller.extend("sap.ui.demo.ui5.controller.App",{
 
         onInit: function(){
             //set o data model ont he view
-            var oData = {
-                "rec":{
-                    "name":"sujit"
+            var oData={
+                rec:{
+                    name:"ui5"
                 }
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            //set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName : "sap.ui.demo.ui5.i18n.i18n",
+                supportedLocales:[""],
+                fallbackLocales:""
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
              onshow: function(){
                 // alert("hellllllllllllll");
-                MessageToast.show("hello");
+                // MessageToast.show("hello");
+                var oBundle= this.getView().getModel("i18n").getResourceBundle();
+                var srec = this.getView().getModel().getProperty("/rec/name");
+                 var sMsg = oBundle.getText("helloMsg", [srec]);
+                MessageToast.show(sMsg);
         }
     });
 
